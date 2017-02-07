9b7f757df27c782e93eb89f12c646df7e022a697
davidkatz <15Dkatz@shcp.edu>
2017-02-07 09:31:04 -0800

    add lectures

---
+++ course_material/lectures/es6_essentials/arrow_functions/index.js
@@ -0,0 +1,22 @@
+// // part 1:
+// function cheer() {
+//   console.log("Woohoo!")
+// }
+
+// // part 2:
+// var cheer = function() {
+//   console.log("Woohoo!")
+// }
+
+// // part 3:
+// setTimeout(() => {
+//   console.log("Woohoo!")
+// }, 3000);
+
+// // part 4:
+// let cheer = () => {
+//   console.log("Woohoo!");
+// }
+
+// // all parts above:
+// cheer();
---
+++ course_material/lectures/es6_essentials/map_filter/index.js
@@ -0,0 +1,36 @@
+// * map helper method
+// parts 1-3:
+// let values = [20, 30, 40];
+
+// // part 1:
+// let double = (n) => {
+//   return n*2;
+// }
+// let doubled = values.map(double);
+
+
+// // part 2:
+// let doubled = values.map((n) => {
+//   return n*2;
+// });
+
+// // part 3:
+// let doubled = values.map(n => n*2);
+
+// all parts above:
+// console.log(doubled);
+
+// * filter helper method
+// parts 4-5:
+// let points = [7, 16, 21, 4, 3, 22, 5];
+
+// part 4:
+// let highScores = points.filter((n) => {
+//   return n > 15;
+// });
+
+// part 5:
+// let highScores = points.filter(n => n>15);
+
+// parts 4-5:
+// console.log(highScores);
