886d8669f5dc4fbfe0f55d96ddfc311e22420dcb
davidkatz <15Dkatz@shcp.edu>
2017-02-06 23:04:49 -0800

    complete destructuring assignment

---
+++ course_material/lectures/es6_essentials/destructuring_assignment_arrays/index.js
@@ -0,0 +1,25 @@
+// // part 1:
+// let z = [4, 5, 6];
+// // let four = z[0];
+// // let five = z[1];
+// let [four, five] = z;
+// console.log(four, five);
+
+// // part 2:
+// let animals = ["Simba", "Zazu", "Ed"];
+// let [lion, bird] = animals;
+// console.log(lion, bird);
+
+// // part 3:
+// let king = { name: 'Mufasa', kids: 1 };
+// // let name = king.name;
+// // let kids = king.kids;
+// let { name, kids } = king;
+// console.log(name, kids);
+
+// // part 4:
+// let son = { name: 'Simba', parents: 2 };
+// let name, parents;
+// // { name, parents } = son; // error; standalone block, not destructuring assigment.
+// ({ name, parents } = son); // valid destructuring assignment
+// console.log(name, parents);
---
+++ course_material/lectures/es6_essentials/spread_rest/index.js
@@ -0,0 +1,21 @@
+// * spread operators
+// part 1:
+// let a = [20, 30, 40];
+// let b = [10, 50];
+
+// part 2:
+// let a = [20, 30, 40];
+// let b = [10, ...a, 50];
+// console.log(b);
+
+// part 3:
+// let a = ['Dana', 'Erik', 'Frank'];
+// let b = ['Alice', 'Bob', 'Carl', ...a];
+// console.log(b);
+
+// * rest parameters
+// function collect(...a) {
+//   console.log(a);
+// }
+//
+// collect(1, 2, 3, 4, 5);
