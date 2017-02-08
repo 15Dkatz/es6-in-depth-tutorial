83f52f7a7b982835a0bf1c1aa1939af0195d8001
davidkatz <15Dkatz@shcp.edu>
2017-02-08 00:09:44 -0800

    add string helper method lecture

---
+++ course_material/lectures/more_builtin_methods/string_helpers/index.js
@@ -0,0 +1,25 @@
+// // part 1:
+// let b = "wooh" + "oo".repeat(50);
+// console.log(b);
+
+// // part 2:
+// let b = "wooh" + " ".repeat(50) + "oo";
+// console.log(b);
+
+// // part 3:
+// console.log("butterfly".startsWith("butter")); // true
+//
+// // part 4:
+// console.log("butterfly".startsWith("fly")); // false
+//
+// // part 5:
+// console.log("butterfly".endsWith("fly")); // true
+//
+// // part 6:
+// console.log("butterfly".includes("fly")); // true
+//
+// // part 7:
+// console.log("butterfly".includes("butter")); // true
+//
+// // part 8:
+// console.log("butterfly".includes("caterpillar")); // false
