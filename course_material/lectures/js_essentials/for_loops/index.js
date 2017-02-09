155a5e2004c7523fab870387f7f41693c69cd58c
davidkatz <15Dkatz@shcp.edu>
2017-02-09 09:59:12 -0800

    add for loops lecture

---
+++ course_material/lectures/js_essentials/for_loops/index.js
@@ -0,0 +1,26 @@
+// // part 1:
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// // ...
+//
+// // part 2:
+// for (var i=0; i<5; i = i+1) {
+//   console.log(i);
+// }
+//
+// // part 3:
+// for (var i=0; i<5; i += 1) {
+//   console.log(i);
+// }
+//
+// // part 4:
+// for (var i=0; i<5; i++) {
+//   console.log(i);
+// }
+//
+// // part 5:
+// var names = ["Frodo", "Sam", "Merry", "Pippin"];
+// for (var i=0; i<names.length; i++) {
+//   console.log(names[i]);
+// }
