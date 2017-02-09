e483ab296b03d892957f4d00a30882bd51c23d6a
davidkatz <15Dkatz@shcp.edu>
2017-02-09 09:18:36 -0800

    add if statements lecture

---
+++ course_material/lectures/js_essentials/if_statements/index.js
@@ -0,0 +1,43 @@
+// // part 1:
+// if (boolean) {
+//
+// }
+
+// // part 2:
+// if (8 > 5) {
+//   console.log("8 is greater!");
+// }
+
+// // part 3:
+// if (8 > 13) {
+//   console.log("8 is greater!");
+// }
+
+// // part 4:
+// if (8 > 13) {
+//   console.log("8 is greater!");
+// } else {
+//   console.log("8 is not greater.");
+// }
+
+// // part 4:
+// var x = 10;
+// if (8 > x) {
+//   console.log("8 is greater!");
+// } else if (15 > x) {
+//   console.log("15 is greater!");
+// } else {
+//   console.log("8 is not greater.");
+// }
+
+// // part 5:
+// function check(number) {
+//   if (number % 2 == 0) {
+//     console.log(number + " is even");
+//   } else {
+//     console.log(number + " is odd");
+//   }
+// }
+//
+// check(4);
+// check(7);
