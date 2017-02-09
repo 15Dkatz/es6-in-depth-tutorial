baad07e7193802caf2ab273b55d04fbb1504e2e6
davidkatz <15Dkatz@shcp.edu>
2017-02-08 18:12:03 -0800

    add lectures

---
+++ course_material/lectures/introducing_js/js_dom/index.js
@@ -0,0 +1,8 @@
+// // part 1:
+// console.log(document);
+
+// // part 2:
+// console.log(typeof document);
+//
+// // part 3:
+// document.getElementById('example').innerHTML = 'JavaScript rules!';
