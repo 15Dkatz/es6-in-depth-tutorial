0fbce3865f4328668d8a7a2694b10020e54d5a27
davidkatz <15Dkatz@shcp.edu>
2017-02-09 10:23:06 -0800

    add while loops lecture

---
+++ course_material/lectures/js_essentials/while_loops/index.js
@@ -0,0 +1,39 @@
+// // all parts:
+var names = ["Frodo", "Sam", "Merry", "Pippin"];
+// for (var i=0; i<names.length; i++) {
+//   console.log(names[i]);
+// }
+//
+// // part 1:
+// var i = 0;
+// while (i < names.length) {
+//   console.log(names[i]);
+//   i++;
+// }
+//
+// // part 2:
+// while (i < names.length) {
+//   console.log(names[i]);
+//   // i++; // infinite loop!
+// }
+//
+// // part 3:
+// var i = 0;
+// do {
+//   console.log(names[i]);
+//   i++;
+// } while (i < names.length);
+//
+// // part 3:
+// var i = 0;
+// do {
+//   console.log(names[i]); // still executed once
+//   i++;
+// } while (i < 0);
+//
+// // part 4:
+// var i = 0;
+// while (i < 0) {
+//   console.log(names[i]); // never reached
+//   i++;
+// }
