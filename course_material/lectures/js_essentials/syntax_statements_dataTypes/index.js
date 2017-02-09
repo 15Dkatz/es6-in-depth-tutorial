0492033a597d19a10baba92826372449422b0d4c
davidkatz <15Dkatz@shcp.edu>
2017-02-08 21:15:08 -0800

    variables and assignment

---
+++ course_material/lectures/js_essentials/syntax_statements_dataTypes/index.js
@@ -0,0 +1,23 @@
+// // part 1:
+// var a, b;
+// a = 'hello';
+// b = a + ' world';
+// // b = 'hello world'
+
+// // part 2:
+// var a, b;
+// a = "hello";
+// b = a + " world";
+// // b = 'hello world'
+
+// // part 3:
+// var a, b;
+// a = 1;
+// b = a + 5.5;
+// // b = 6.5;
+
+// // part 4:
+/*
+This comment
+spans multiple lines
+*/
---
+++ course_material/lectures/js_essentials/variables_and_assignment/index.js
@@ -0,0 +1,20 @@
+// // part 1:
+// var a = 30;
+
+// // part 2:
+// var var = 30;
+
+// // part 3:
+// var 1a = 30;
+
+// // part 4:
+// var $_a = 30;
+
+// // part 5:
+// var first greeting = 'hello';
+
+// // part 6:
+// var first_greeting = 'hello';
+
+// // part 7:
+// var firstGreeting = 'hello';
