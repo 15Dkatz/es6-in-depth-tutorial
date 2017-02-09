d90e7f7f71517680c534d29ddcef957a6ce2e5c0
davidkatz <15Dkatz@shcp.edu>
2017-02-09 09:35:49 -0800

    add switch statements lecture

---
+++ course_material/lectures/js_essentials/switch_statements/index.js
@@ -0,0 +1,41 @@
+// // part 1:
+// var x = 3;
+// switch(x) {
+//   case 1:
+//     console.log("The number is one!");
+//   case 2:
+//     console.log("The number is two!");
+//   case 3:
+//     console.log("The number is three!");
+// }
+//
+// // part 2:
+// var x = 1;
+// switch(x) {
+//   case 1:
+//     console.log("The number is one!");
+//     break;
+//   case 2:
+//     console.log("The number is two!");
+//     break;
+//   case 3:
+//     console.log("The number is three!");
+//     break;
+// }
+//
+// // part 3:
+// var x = 13;
+// switch(x) {
+//   case 1:
+//     console.log("The number is one!");
+//     break;
+//   case 2:
+//     console.log("The number is two!");
+//     break;
+//   case 3:
+//     console.log("The number is three!");
+//     break;
+//   default:
+//     console.log("The number is " + x);
+//     break;
+// }
