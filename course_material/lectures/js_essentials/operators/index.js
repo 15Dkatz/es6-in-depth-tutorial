ef0b70e5520883ca82021a7cc103b72c986716c0
davidkatz <15Dkatz@shcp.edu>
2017-02-08 21:36:37 -0800

    add operators

---
+++ course_material/lectures/js_essentials/operators/index.js
@@ -0,0 +1,19 @@
+// // part 1:
+// var z = 10 + 5 - 2;
+// console.log(z);
+
+// // part 2:
+// var z = 10 * 5 / 2;
+// console.log(z);
+
+// // part 3:
+// var a = "Good" + " " + "day";
+// console.log(a);
+
+// // part 4:
+// var b = 11 % 3;
+// console.log(b);
+
+// // // part 5:
+// var b = 9 % 3;
+// console.log(b);
