c936c8907d403f2bebe92cb41e576cfe5e7677b6
davidkatz <15Dkatz@shcp.edu>
2017-02-10 07:52:38 -0800

    add static methods

---
+++ course_material/lectures/classes/static_methods/index.js
@@ -0,0 +1,24 @@
+// // part 1:
+// class Calculator {
+//   static multiply(a, b) {
+//     return a*b;
+//   }
+// }
+//
+// let a = Calculator.multiply(5, 7);
+// console.log(a);
+
+
+// // part 2:
+// class Calculator {
+//   static add(a, b) {
+//     return a+b;
+//   }
+//
+//   static multiply(a, b) {
+//     return a*b;
+//   }
+// }
+//
+// let a = Calculator.add(5, 7);
+// console.log(a);
