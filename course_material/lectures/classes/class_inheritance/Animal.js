cc5423c3bcdab00e3f6411b485e0fc087bb03bc2
davidkatz <15Dkatz@shcp.edu>
2017-02-10 07:20:06 -0800

    add inheritance lecture

---
+++ course_material/lectures/classes/class_inheritance/Animal.js
@@ -0,0 +1,12 @@
+// class Animal {
+//   constructor(name, height) {
+//     this.name = name;
+//     this.height = height;
+//   }
+//
+//   hello() {
+//     console.log(`Hi! I'm ${this.name} from the Animal kingdom!`);
+//   }
+// }
+//
+// export default Animal;
---
+++ course_material/lectures/classes/class_inheritance/index.js
@@ -0,0 +1,63 @@
+// // parts 1-3:
+// class Animal {
+//   constructor(name, height) {
+//     this.name = name;
+//     this.height = height;
+//   }
+//
+//   hello() {
+//     console.log(`Hi! I'm ${this.name} from the Animal kingdom!`);
+//   }
+// }
+
+
+// // part 1:
+// class Lion extends Animal {
+//
+// }
+//
+// let son = new Lion("Simba", 2);
+// console.log(son);
+
+// // part 2:
+// class Lion extends Animal {
+//   constructor(name, height, color) {
+//     super(name, height);
+//     this.color = color;
+//   }
+// }
+//
+// let son = new Lion("Simba", 2, "golden");
+// console.log(son);
+
+// // part 3:
+// class Lion extends Animal {
+//   constructor(name, height, color) {
+//     super(name, height);
+//     this.color = color;
+//   }
+//
+//   hello() {
+//     console.log(`Hi! I'm ${this.name} from Pride Rock!`);
+//   }
+// }
+//
+// let son = new Lion("Simba", 2, "golden");
+// son.hello();
+
+// // part 4:
+// import Animal from './Animal';
+//
+// class Lion extends Animal {
+//   constructor(name, height, color) {
+//     super(name, height);
+//     this.color = color;
+//   }
+//
+//   hello() {
+//     console.log(`Hi! I'm ${this.name} from Pride Rock!`);
+//   }
+// }
+//
+// let son = new Lion("Simba", 2, "golden");
+// son.hello();
