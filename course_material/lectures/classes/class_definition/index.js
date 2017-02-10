82970df9578034e771aa40aff3cdb2d85a9a0d3a
davidkatz <15Dkatz@shcp.edu>
2017-02-10 00:40:40 -0800

    add class definition lecture

---
+++ course_material/lectures/classes/class_definition/index.js
@@ -0,0 +1,25 @@
+// part 1:
+// class Animal {
+//   constructor(name, height) {
+//     this.name = name;
+//     this.height = height;
+//   }
+// }
+//
+// var king = new Animal("Mufasa", 4.5);
+// console.log(king);
+//
+// part 2:
+// class Animal {
+//   constructor(name, height) {
+//     this.name = name;
+//     this.height = height;
+//   }
+//
+//   hello() {
+//     console.log(`Hi! I'm ${this.name} from the Animal kingdom!`);
+//   }
+// }
+//
+// var king = new Animal("Mufasa", 4.5);
+// king.hello();
