9efde710b0a80523269eea7ebb35609c69084b58
davidkatz <15Dkatz@shcp.edu>
2017-02-10 20:17:35 -0800

    update prototypes lecture

--- course_material/lectures/classes/prototypes/index.js
+++ course_material/lectures/classes/prototypes/index.js
@@ -31,3 +31,31 @@
 // let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
 // harry.pet_name = "Hedwig";
 // console.log(harry);
+
+// parts 4-5:
+// function Wizard(name, house, pet) {
+//   this.name = name;
+//   this.house = house;
+//   this.pet = pet;
+//   this.greet = () => `I'm ${this.name} from ${this.house}`;
+// }
+
+// // part 4:
+// Wizard.prototype.pet_name;
+// Wizard.prototype.info;
+// let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
+// harry.pet_name = "Hedwig";
+// harry.info = () => {
+//   return `I have a ${this.pet} named ${this.pet_name}.`;
+// }
+// console.log(harry.info());
+
+// // part 4:
+// Wizard.prototype.pet_name;
+// Wizard.prototype.info;
+// let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
+// harry.pet_name = "Hedwig";
+// harry.info = function() {
+//   return `I have a ${this.pet} named ${this.pet_name}.`;
+// }
+// console.log(harry.info());
