e45c55ef032315972f2e44c51a88679b991c9c27
davidkatz <15Dkatz@shcp.edu>
2017-02-10 17:55:37 -0800

    add prototypes lecture

---
+++ course_material/lectures/classes/prototypes/index.js
@@ -0,0 +1,33 @@
+// // part 1:
+// function Wizard(name, house, pet) {
+//   this.name = name;
+//   this.house = house;
+//   this.pet = pet;
+// }
+//
+// let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
+// console.log(harry);
+
+// // part 2:
+// function Wizard(name, house, pet) {
+//   this.name = name;
+//   this.house = house;
+//   this.pet = pet;
+//   this.greet = () => `I'm ${this.name} from ${this.house}`;
+// }
+//
+// let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
+// console.log(harry.greet());
+
+// // part 3:
+// function Wizard(name, house, pet) {
+//   this.name = name;
+//   this.house = house;
+//   this.pet = pet;
+//   this.greet = () => `I'm ${this.name} from ${this.house}`;
+// }
+//
+// Wizard.prototype.pet_name;
+// let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
+// harry.pet_name = "Hedwig";
+// console.log(harry);
