7951f36db14320a11ccdd30df3f7d9c0b2713f5c
davidkatz <15Dkatz@shcp.edu>
2017-02-11 10:07:24 -0800

    add function factories lecture

---
+++ course_material/lectures/closures/function_factories/index.js
@@ -0,0 +1,39 @@
+// // part 1:
+// const addSuffix = (x) => {
+//   const concat = (y) => {
+//     return y + x;
+//   }
+//   return concat;
+// }
+//
+// let add_ness = addSuffix("ness");
+// console.log(add_ness);
+// let h = add_ness("happi");
+// console.log(h);
+
+// // part 2:
+// const product = (x) => {
+//   return y => {
+//     return x * y;
+//   }
+// };
+//
+// let mult5 = product(5);
+// console.log(mult5(3));
+//
+// let double = product(2);
+// console.log(double(9));
+
+// // part 3:
+// const product = (x) => {
+//   return y => x * y;
+// };
+//
+// let double = product(2);
+// console.log(double(9));
+//
+// // part 4
+// const product = (x) => y => x * y;
+//
+// let double = product(2);
+// console.log(double(9));
