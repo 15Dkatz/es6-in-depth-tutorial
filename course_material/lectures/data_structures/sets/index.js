6b991b355e7cd4c17583ae94a00a35b9961fcae8
davidkatz <15Dkatz@shcp.edu>
2017-02-10 21:57:22 -0800

    add sets lecture

---
+++ course_material/lectures/data_structures/sets/index.js
@@ -0,0 +1,39 @@
+// // all parts 1-4:
+// var a = new Set();
+//
+// a.add(5);
+// a.add(43);
+//
+// a.add("woohoo");
+//
+// a.add({ x: 50, y: 200 });
+
+// // part 1:
+// console.log(a);
+
+// part 2:
+// console.log(a.size);
+//
+// // part 3:
+// console.log(a.has(5));
+//
+// // part 4:
+// console.log(a.has(7));
+
+// // parts 5-6:
+// let numbers = [5, 7, 8, 13, 17];
+// let numSet = new Set(numbers);
+//
+// // // part 5:
+// // console.log(numSet);
+//
+// // part 6:
+// for (let element of numSet.values()) {
+//   console.log(element);
+// }
+//
+// // part 7:
+// let chars = "aweifawjeoanehmwofeuawhfhwuei";
+// let chars_arr = chars.split("");
+// let chars_set = new Set(chars_arr);
+// console.log(chars_set);
