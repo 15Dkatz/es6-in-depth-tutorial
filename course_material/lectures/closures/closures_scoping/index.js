26bce99738441b859e80a3ddb1dbe616b7689e2c
davidkatz <15Dkatz@shcp.edu>
2017-02-11 09:21:08 -0800

    add closures lecture

---
+++ course_material/lectures/closures/closures_scoping/index.js
@@ -0,0 +1,28 @@
+// // part 1:
+// let call = () => {
+//   let secret = 'ES6 rocks';
+// }
+// console.log(secret); // error
+
+// part 2:
+// let call = () => {
+//   let secret = 'ES6 rocks';
+//   let reveal = () => {
+//     console.log(secret);
+//   }
+//   reveal();
+// }
+//
+// call();
+//
+// // part 3:
+// let call = () => {
+//   let secret = 'ES6 rocks';
+//   let reveal = () => {
+//     console.log(secret);
+//   }
+//   return reveal;
+// }
+//
+// let unveil = call();
+// unveil();
