9010d8655d74351be82e324edf1e576d28b08441
davidkatz <15Dkatz@shcp.edu>
2017-02-12 20:31:22 -0800

    add fetch lecture

---
+++ course_material/lectures/promises/fetch/index.js
@@ -0,0 +1,8 @@
+// const root = 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699';
+//
+// fetch(root, {method: 'GET'})
+//   .then(response => {
+//     // console.log(response);
+//     return response.json();
+//   })
+//   .then(json => console.log(json));
