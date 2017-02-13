de760d9f27663b57763aa95508ac82a8f7a8adea
davidkatz <15Dkatz@shcp.edu>
2017-02-12 19:27:00 -0800

    add promise creation lecture

---
+++ course_material/lectures/promises/promise_creation/index.js
@@ -0,0 +1,22 @@
+// // part 1:
+// let p = new Promise((resolve, reject) => {
+//   resolve('Resolved promise data');
+// });
+//
+// p.then(response => console.log(response));
+
+// // part 2:
+// let p = new Promise((resolve, reject) => {
+//   reject('Rejected promise data');
+// });
+//
+// p.then(response => console.log(response))
+//  .catch(error => console.log(error));
+
+// // part 3:
+// let p = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve('Resolved promise data'), 3000);
+// });
+//
+// p.then(response => console.log(response));
+// console.log("after promise consumption");
