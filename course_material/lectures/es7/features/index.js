5f2725d7eaa30c79d773a448f5dca073f9e094ab
davidkatz <15Dkatz@shcp.edu>
2017-02-13 21:02:22 -0800

    add es7 features lecture

---
+++ course_material/lectures/es7/features/index.js
@@ -0,0 +1,15 @@
+// // part 1:
+// let a = Math.pow(2, 5);
+// console.log(a);
+
+// // part 2:
+// let a = 2**5;
+// console.log(a);
+
+// // part 3:
+// let b = "wonderful".includes("wonder");
+// console.log(b);
+
+// // part 3:
+// let b = [2, 3, 4, 5, 6].includes(7);
+// console.log(b);
