7d2fe5b92b0d3e8279d0053a9b43f2ffcf6d9458
davidkatz <15Dkatz@shcp.edu>
2017-02-13 21:46:36 -0800

    add es proposal objects lecture

---
+++ course_material/lectures/es7/proposals_objects/index.js
@@ -0,0 +1,23 @@
+// // parts 1-2:
+// let obj = {
+//   a: 'one',
+//   b: 'two',
+//   c: 'three'
+// }
+//
+// // part 1:
+// let keys = Object.keys(obj);
+// console.log(keys);
+//
+// // part 2:
+// let values = Object.values(obj);
+// console.log(values);
+//
+// // part 3:
+// let entries = Object.entries(obj);
+// console.log(entries);
+//
+// // part 4:
+// for (let entry of entries) {
+//   console.log(`key: ${entry[0]} | value: ${entry[1]}`);
+// }
