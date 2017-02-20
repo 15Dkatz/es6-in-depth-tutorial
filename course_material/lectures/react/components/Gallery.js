e50397da8ab5b5748d896f85462c4834550a3c57
davidkatz <15Dkatz@shcp.edu>
2017-02-20 08:16:33 -0800

    update alternate

--- course_material/lectures/react/components/Gallery.js
+++ course_material/lectures/react/components/Gallery.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
   render() {
+    let alternate = 'https://cdn0.iconfinder.com/data/icons/thin-photography/57/thin-367_photo_image_wall_unavailable_missing-512.png';
+
     return (
       <div>
         {
@@ -15,7 +17,7 @@ class Gallery extends Component {
                 target="_blank"
               >
                 <img
-                  src={imageLinks !== undefined ? imageLinks.thumbnail : ""}
+                  src={imageLinks !== undefined ? imageLinks.thumbnail : alternate}
                   className="book-img"
                   alt="book"
                 />
