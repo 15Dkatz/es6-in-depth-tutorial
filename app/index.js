9b24d2270b39bbfc29446ca1956af62f84e9d41b
Bello Raafi <bellsraafi@gmail.com>
2019-01-17 10:59:48 +0100

    Added .travis.yml file

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Global from "./components/Global";
+
+ReactDOM.render(
+  <Global />,
+  document.getElementById("root")
+);
--- course_material/lectures/introducing_js/adding_js_to_html/index.js
+++ course_material/lectures/introducing_js/adding_js_to_html/index.js
@@ -1,1 +1,2 @@
-console.log("Hello from JavaScript");
+document.getElementById('example').innerHTML = 'JavaScript rules!';
+console.log([1,2,3].map(n=> n+1));
